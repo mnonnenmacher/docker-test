2a28691650a8a3eea650b5792f70ea40ab2e2e28
Martin Nonnenmacher <martin.nonnenmacher@gmail.com>
2024-02-25 22:29:51 +0100

    Add test application

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    application
+    kotlin("jvm") version "1.9.22"
+    alias(libs.plugins.jib)
+//    alias(libs.plugins.kotlinJvm)
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+application {
+    applicationName = "docker-test"
+    mainClass = "org.example.MainKt"
+}
+
+jib {
+    from.image = "eclipse-temurin:${libs.versions.eclipseTemurin.get()}"
+    to.image = "docker-test:latest"
+
+    container {
+        mainClass = "org.example.MainKt"
+    }
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,20 @@
+package org.example
+
+fun main() {
+    println("Start")
+    run("cargo", "--version")
+    run("java", "-version")
+    run("npm", "--version")
+}
+
+private fun run(vararg command: String) {
+    runCatching {
+        val builder = ProcessBuilder(*command)
+        builder.redirectErrorStream(true)
+        val process = builder.start()
+        val output = process.inputStream.bufferedReader().readText()
+        println("\n${command.joinToString()}:\n$output\n")
+    }.onFailure {
+        println("\nError: ${it.message}\n")
+    }
+}
